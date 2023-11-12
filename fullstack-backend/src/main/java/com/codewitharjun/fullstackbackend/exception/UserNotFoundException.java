652721addf1b9e31c0ddbcdf63076969265db65a
JoaoMartins988 <j.martins264@gmail.com>
2023-11-12 02:11:14 +0000

    HOT-12

--- fullstack-backend/src/main/java/com/codewitharjun/fullstackbackend/exception/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.codewitharjun.fullstackbackend.exception;
-
-
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(Long id){
-        super("Could not found the car with id "+ id);
-    }
-}
