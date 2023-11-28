b26f1f58a9d078d57089c6d35d786cad40f1d2c9
JoaoMartins988 <j.martins264@gmail.com>
2023-11-28 15:47:09 +0000

    HOT-2 sort by

--- fullstack-front/src/layout/Navbar.js
+++ fullstack-front/src/layout/Navbar.js
@@ -2,12 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
-  const [searchQuery, setSearchQuery] = useState('');
- 
-  const handleSearch = () => {
-   console.log('Pesquisando por:', searchQuery);
-  };
-
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar- dark bg-primary">
@@ -26,21 +20,6 @@ export default function Navbar() {
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="form-inline">
-            <input
-              type="text"
-              className="search-box"
-              placeholder="Search..."
-              value={searchQuery}
-              onChange={(e) => setSearchQuery(e.target.value)}
-            />
-            <button
-              className="btn btn-outline-light"
-              onClick={handleSearch}
-            >
-              Pesquisar
-            </button>
-          </div>
           <Link className="btn btn-outline-light" to="/addCar" >
             Add Cars
           </Link>
--- fullstack-front/src/pages/Home.js
+++ fullstack-front/src/pages/Home.js
@@ -1,86 +1,165 @@
 import React, { useEffect, useState } from 'react';
 import axios from "axios";
-import { Link, useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
   const [cars, setCars] = useState([]);
   const [totalCars, setTotalCars] = useState(10);
-  const {id} = useParams();
   const [currentPage, setCurrentPage] = useState(1);
   const PAGE_SIZE = 10;
+  const [sortBy, setSortBy] = useState(null);
+  const [sortOrder, setSortOrder] = useState('asc');
+  const [searchQuery, setSearchQuery] = useState('');
 
- 
+  const handleSearch = () => {
+    console.log('Pesquisando por:', searchQuery);
+  };
+
   useEffect(() => {
     loadCars(currentPage);
-  },[currentPage]) ;
-const loadCars = async (page) => {
-const result2 = await axios.get(`http://localhost:8080/carrinhos`);
-const result = await axios.get(`http://localhost:8080/carrinhos/p?page=${page}&pageSize=${PAGE_SIZE}`);
-const sortedCars = result.data.sort((a, b) => a.marca.localeCompare(b.marca));
-setTotalCars(result2.data.length);
-setCars(sortedCars);
-};
-function getTotalCars() {
-  return totalCars;
-}
-const deleteCars = async (id)=>{
-  const response = await axios.delete(`http://localhost:8080/carrinho/${id}`);
-  alert(response.data);
-  loadCars(currentPage);
-};
-const handlePageChange = (newPage) => {
-  setCurrentPage(newPage);
-};
+  }, [currentPage, sortBy, sortOrder]);
 
-return (
+  const loadCars = async (page) => {
+    try {
+      const result2 = await axios.get(`http://localhost:8080/carrinhos`);
+      const result = await axios.get(`http://localhost:8080/carrinhos/p`, {
+        params: {
+          page: page,
+          pageSize: PAGE_SIZE,
+          sortBy: sortBy,
+          sortOrder: sortOrder,
+        },
+      });
+
+      let sortedCars = result.data;
+
+      if (sortBy) {
+        sortedCars.sort((a, b) => {
+          const valueA = a[sortBy];
+          const valueB = b[sortBy];
+
+          return sortOrder === 'asc' ? valueA.localeCompare(valueB) : valueB.localeCompare(valueA);
+        });
+      }
+
+      setTotalCars(result2.data.length);
+      setCars(sortedCars);
+    } catch (error) {
+      console.error("Error loading cars:", error);
+    }
+  };
+
+  const handleSort = (columnName) => {
+    if (sortBy === columnName) {
+      setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
+    } else {
+      setSortBy(columnName);
+      setSortOrder('asc');
+    }
+  };
+
+  const deleteCars = async (id) => {
+    const response = await axios.delete(`http://localhost:8080/carrinho/${id}`);
+    alert(response.data);
+    loadCars(currentPage);
+  };
+
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
+
+  return (
     <div className='container'>
-      <h4>Total Cars: {totalCars}</h4>
+      <h4>Total Carros: {totalCars}</h4>
       <div className='py-4'>
+        <div className="form-inline">
+          <input
+            type="text"
+            className="search-box"
+            placeholder="procurar..."
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+          <button
+            className="btn btn-primary mx-2"
+            onClick={handleSearch}
+          >
+            Pesquisa
+          </button>
+        </div>
+        <p> </p>
         <table className="table border shadow">
           <thead>
             <tr>
               <th scope="col">Number</th>
-              <th scope="col">Marca</th>
-              <th scope="col">Modelo</th>
-              <th scope="col">Cor</th>
-              <th scope="col">Ano</th>           
+              <th scope="col">
+                <div>
+                  <span>Marca</span>
+                  <button className="btn btn-link" onClick={() => handleSort("marca")}>
+                    {sortBy === "marca" && sortOrder === "asc" ? "▲" : "▼"}
+                  </button>
+                </div>
+              </th>
+              <th scope="col">
+                <div>
+                  <span>Modelo</span>
+                  <button className="btn btn-link" onClick={() => handleSort("modelo")}>
+                    {sortBy === "modelo" && sortOrder === "asc" ? "▲" : "▼"}
+                  </button>
+                </div>
+              </th>
+              <th scope="col">
+                <div>
+                  <span>Cor</span>
+                  <button className="btn btn-link" onClick={() => handleSort("cor")}>
+                    {sortBy === "cor" && sortOrder === "asc" ? "▲" : "▼"}
+                  </button>
+                </div>
+              </th>
+              <th scope="col">
+                <div>
+                  <span>Ano</span>
+                  <button className="btn btn-link" onClick={() => handleSort("ano")}>
+                    {sortBy === "ano" && sortOrder === "asc" ? "▲" : "▼"}
+                  </button>
+                </div>
+              </th>
               <th scope="col">Action</th>
             </tr>
           </thead>
           <tbody>
-            {
-              cars.map((car, index) => (
-                <tr>
-                  <th scope="row" key={index}>{index + 1}</th>
-                  <td>{car.marca}</td>
-                  <td>{car.modelo}</td>
-                  <td>{car.cor}</td>
-                  <td>{car.ano}</td>                 
-                  <td>
-                  <Link className="btn btn-primary mx-2"to={`/viewCar/${car.id}`}>View</Link>
-                  <Link className="btn btn-outline-primary mx-2"to={`/editCar/${car.id}`}>Edit</Link>
-                  <button className="btn btn-danger mx-2" onClick={()=>deleteCars(car.id)}>Delete</button>
-                  </td>
-                </tr>
-              ))}
+            {cars.map((car, index) => (
+              <tr key={index}>
+                <th scope="row">{index + 1}</th>
+                <td>{car.marca}</td>
+                <td>{car.modelo}</td>
+                <td>{car.cor}</td>
+                <td>{car.ano}</td>
+                <td>
+                  <Link className="btn btn-primary mx-2" to={`/viewCar/${car.id}`}>View</Link>
+                  <Link className="btn btn-outline-primary mx-2" to={`/editCar/${car.id}`}>Edit</Link>
+                  <button className="btn btn-danger mx-2" onClick={() => deleteCars(car.id)}>Delete</button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
       <div>
-          <span>Página {currentPage} de {Math.ceil(totalCars / PAGE_SIZE)}</span>
-          <button
-            disabled={currentPage === 1}
-            onClick={() => handlePageChange(currentPage - 1)}
-          >
-            Anterior
-          </button>
-          <button
-            disabled={currentPage === Math.ceil(totalCars / PAGE_SIZE)}
-            onClick={() => handlePageChange(currentPage + 1)}
-          >
-            Próxima
-          </button>
-        </div>
+        <span>Página {currentPage} de {Math.ceil(totalCars / PAGE_SIZE)}</span>
+        <button
+          disabled={currentPage === 1}
+          onClick={() => handlePageChange(currentPage - 1)}
+        >
+          Anterior
+        </button>
+        <button
+          disabled={currentPage === Math.ceil(totalCars / PAGE_SIZE)}
+          onClick={() => handlePageChange(currentPage + 1)}
+        >
+          Próxima
+        </button>
       </div>
+    </div>
   );
-};
+}
